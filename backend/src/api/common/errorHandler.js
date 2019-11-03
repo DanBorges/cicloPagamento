5e7f0c9e1ec6b3e957b812b73db61a21a4f624ee
Daniel Borges <daniel.lemes.borges@hmail.com>
2019-11-03 12:21:46 -0300

    finalizando backend

---
+++ backend/src/api/common/errorHandler.js
@@ -0,0 +1,17 @@
+const _ = require('lodash')
+
+module.exports = (req, res, next) => {
+    const bundle = res.locals.bundle
+    if (bundle.errors) {
+        const errors = parseErrors(bundle.errors)
+        res.ststus(500).json({errors})
+    } else {
+        next()
+    }
+}
+
+const parseErrors = (nodeRestfullErrors) => {
+    const errors = []
+    _.forIn(nodeRestfullErrors, error => errors.push(error.message))
+    return errors
+}
