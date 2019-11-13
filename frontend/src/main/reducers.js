2034dd76deaf4802bfb653ecbcfee02c4da1bca7
Daniel Borges <daniel.a.borges@capgemini.com>
2019-11-13 12:37:01 +0000

    Contruindo redux para exibição de lista de billingCycles

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCylcleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billyngCycle: BillingCylcleReducer
 })
 
 export default rootReducer
