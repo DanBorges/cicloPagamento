1b0570103e205708063a55fd84d73ee755646ca4
Daniel Borges <daniel.a.borges@capgemini.com>
2019-11-14 13:07:43 +0000

    Add mensagens de erro

--- backend/src/api/common/errorHandler.js
+++ backend/src/api/common/errorHandler.js
@@ -2,16 +2,17 @@ const _ = require('lodash')
 
 module.exports = (req, res, next) => {
     const bundle = res.locals.bundle
-    if (bundle.errors) {
+
+    if(bundle.errors) {
         const errors = parseErrors(bundle.errors)
-        res.ststus(500).json({errors})
+        res.status(500).json({errors})
     } else {
         next()
     }
 }
 
-const parseErrors = (nodeRestfullErrors) => {
+const parseErrors = (nodeRestfulErrors) => {
     const errors = []
-    _.forIn(nodeRestfullErrors, error => errors.push(error.message))
-    return errors
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+    return errors 
 }
